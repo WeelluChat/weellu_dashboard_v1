1106b699f39af9c0c3c00c5b51f8eeae4e810200
rikeealmeida <henrique.almeida.ads@gmail.com>
2022-05-09 17:58:38 -0300

    woorking

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -1,9 +1,11 @@
 import 'package:dashboard_v1/constants/controllers.dart';
 import 'package:dashboard_v1/helpers/responsiveness.dart';
+import 'package:dashboard_v1/pages/overview/widgets/available_drivers.dart';
 import 'package:dashboard_v1/pages/overview/widgets/overview_cards_large.dart';
 import 'package:dashboard_v1/pages/overview/widgets/overview_cards_medium.dart';
 import 'package:dashboard_v1/pages/overview/widgets/overview_cards_small.dart';
 import 'package:dashboard_v1/pages/overview/widgets/revenue_section_large.dart';
+import 'package:dashboard_v1/pages/overview/widgets/revenue_section_small.dart';
 import 'package:dashboard_v1/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
@@ -39,7 +41,11 @@ class OverViewPage extends StatelessWidget {
                 OverviewCardsLargeScreen()
             else
               OverViewCardsSmallScreen(),
-            RevenueSectionLarge()
+            if (!ResponsiveWidget.isSmallScreen(context))
+              RevenueSectionLarge()
+            else
+              RevenueSectionSmall(),
+            AvailableDrivers()
           ],
         ))
       ],
